cfdc1d9d6085ab6751fd3b091d181439ab728c1b
Vojtěch Marek <vojtech.marek@artiisgroup.cz>
2023-05-15 23:35:17 +0200

    prettier edited, added scss and meta info

---
+++ src/lib/data/meta.ts
@@ -0,0 +1,16 @@
+// theese are constants for meta tags used via <svelte:head>
+
+export const siteBaseUrl = 'https://vojtechmarek.dev';
+
+export const keywords = [
+	'Vojtěch Marek',
+	'Vojtech Marek',
+	'Web Development',
+	'Front-End Development',
+	'Open Source'
+];
+
+export const description =
+	"This is a todo description please change me";
+
+export const title = 'Vojtěch Marek';

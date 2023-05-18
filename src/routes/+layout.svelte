481fa68b67bd927ca5106e6c74bb921dce76027e
Vojtěch Marek <vojtech.marek@artiisgroup.cz>
2023-05-18 15:48:48 +0200

    layout prototyped, added progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,68 +1,108 @@
 <script>
-  import { description, keywords, title, siteBaseUrl } from '$lib/data/meta';
-  import '$lib/scss/global.scss';
+    import { description, keywords, title, siteBaseUrl } from '$lib/data/meta';
+    import '$lib/scss/global.scss';
 </script>
 
 <svelte:head>
-  <link rel="“canonical”" href={siteBaseUrl} />
-  <meta name="keywords" content={keywords.join(', ')} />
+    <link rel="“canonical”" href={siteBaseUrl} />
+    <meta name="keywords" content={keywords.join(', ')} />
 
-  <meta name="description" content={description} />
-  <meta property="og:description" content={description} />
-  <title>{title}</title>
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    <title>{title}</title>
 </svelte:head>
 
-<div class="container">
-  <div class="header">
-    <h1>Header</h1>
-    <p class="description">Page description goes here</p>
-    <h1>Hi, I'm Vojtěch</h1>
+<div>
+    <!-- A skip todo -->
 
-    <div class="subtitle">
-      This is the first version and very much in progress version of my personal website. So far I
-      have
-      <li>Designed initial website ui in Figma</li>
-      <li>Determined color pallete</li>
-      <li>Selected icons</li>
+    <!-- Top area -->
+    <div class="top-area">
+        <!-- above header -->
+        <div class="above-header">above header</div>
+        <div class="header-part">header here</div>
+        <!-- SVG div -->
+        <div class="mountain-part">
+            <svg
+                class="transition-mountain"
+                viewBox="0 0 1499 169"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M363.507 25.4998L6.50684 78.4998V155H1489.51V94.9991L1065.01 25.4998L884.507 78.4998L770.007 25.4998L453.007 78.4998L363.507 25.4998Z"
+                    fill="#4433FF"
+                />
+                <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M131.279 3.45963L336.506 81.1218L481.492 13.4552L523.674 31.1335L549.815 22.9077L641.086 68.1218L698.176 33.7147L787.378 54.7889L816.003 97.1218L877.524 50.3199L1012.73 9.23951L1165.33 97.1218L1261.83 -0.000690558L1498.44 40.6196L1498.44 168.5L727.5 168.5H0.426374L0.426417 64.815L53.8948 84.6221L131.279 3.45963Z"
+                    fill="#F3F2FF"
+                />
+            </svg>
+        </div>
+        <div>my hero and kernel will live here</div>
+        <div>avatar</div>
     </div>
-    I have also started learning Svelte as I will be using it to build website Svelte learning progress
-  </div>
-
-  <svg class="transition-layer" viewBox="0 0 1499 169" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M363.507 25.4998L6.50684 78.4998V155H1489.51V94.9991L1065.01 25.4998L884.507 78.4998L770.007 25.4998L453.007 78.4998L363.507 25.4998Z" fill="#4433FF"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M131.279 3.45963L336.506 81.1218L481.492 13.4552L523.674 31.1335L549.815 22.9077L641.086 68.1218L698.176 33.7147L787.378 54.7889L816.003 97.1218L877.524 50.3199L1012.73 9.23951L1165.33 97.1218L1261.83 -0.000690558L1498.44 40.6196L1498.44 168.5L727.5 168.5H0.426374L0.426417 64.815L53.8948 84.6221L131.279 3.45963Z" fill="#F3F2FF"/>
-  </svg>    
-  <main>
-    <slot />
-  </main>
+    <div><!-- Skip here a todo --></div>
+    <!-- Main area -->
+    <div class="main-area">
+        <main>
+            <slot class="container" />
+        </main>
+    </div>
+    <div><!-- spacer --></div>
+    <footer>this is my foot</footer>
 </div>
 
 <style>
-    .transition-layer {
-       
-    margin-top: -2vh !important; /* TODO not working Blend with element underneath */
-    width: 100vw;
-    height: 169;
-    z-index: 9999;
-  }
-    
-  .container {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
+    .top-area {
+        background-color: white;
+        height: 400px;
+    }
 
-  .header {
-    background-color: white;
-    padding: 20px;
-  }
+    .above-header {
+        min-width: 48px;
+        min-height: 48px;
+    }
 
-  .description {
-    margin-bottom: 20px;
-  }
+    .header-part {
+        position: sticky;
+        top: 0px;
+        z-index: 2;
+        width: 100%;
+        max-width: 1100px;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 32px;
+        padding-right: 32px;
+    }
 
-  main {
-    flex-grow: 1;
-    background-color: #F3F2FF;
-  }
+    .mountain-part {
+        overflow: hidden;
+        display: block;
+        position: absolute;
+        left: 0px;
+        right: 0px;
+        bottom: 300px;
+        width: 100vw;
+        height: 170px;
+        z-index: 3;
+    }
+
+    .transition-mountain {
+        position: absolute;
+left: -3%;
+right: -3%;
+bottom: 0px;
+width: 106%;
+min-width: 600px;
+max-width: unset;
+    }
+
+    .main-area {
+        padding-top: 100px;
+        background-color: #f3f2ff;
+        position: relative;
+        z-index: 3;
+    }
 </style>

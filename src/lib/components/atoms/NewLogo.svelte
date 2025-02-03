2ceb100c724139bc8fb5cceb238704f3bb9c21ce
Vojtěch Marek <32781008+vojtechmarek-dev@users.noreply.github.com>
2025-02-03 14:41:41 +0100

    added new logo svg

---
+++ src/lib/components/atoms/NewLogo.svelte
@@ -0,0 +1,11 @@
+<svg width="628" height="242" viewBox="0 0 628 242" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M112.526 106.931L15.5 186.5L0 170.5L73.4743 107.069L0.5 43.5L15.5 28L112.526 106.931Z" fill="black"/>
+    <path d="M469 231H628" stroke="black" stroke-opacity="0.93" stroke-width="21">
+    <animate attributeName="visibility"
+             values="visible;hidden;visible"
+             dur="1.5s"
+             repeatCount="indefinite"
+             calcMode="discrete"/></path>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M152.5 0L213 184H231.5H247.5L309.674 0H283L231.5 160.5L181 0H152.5Z" fill="black" fill-opacity="0.81"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M326.221 0L317 184H341L345.5 29.5L379.5 124.5H388.286H396.5L432 29.5L437.5 184H462.5L453.5 0H423L388.286 91L356 0H326.221Z" fill="black" fill-opacity="0.88"/>
+</svg>
